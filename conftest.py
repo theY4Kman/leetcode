3840d0a923a74ea339a2b06b0f402e6a634209a9
Zach Kanzler <they4kman@gmail.com>
2014-12-29 01:27:24 -0500

    Allow multiple arguments in test cases

--- conftest.py
+++ conftest.py
@@ -11,9 +11,16 @@ def pytest_pycollect_makeitem(collector, name, obj):
         # The solution will be the only defined method on the class
         attrname, _ = inspect.getmembers(obj, inspect.ismethod)[0]
 
-        @pytest.mark.parametrize(['input', 'expected'], obj.tests)
-        def test(input, expected):
-            assert getattr(obj(), attrname)(input) == expected
+        cases = obj.tests
+        if not cases:
+            return
+
+        # Split into (args, expected)
+        cases = [(c[:-1], c[-1]) for c in cases]
+
+        @pytest.mark.parametrize(['args', 'expected'], cases)
+        def test(args, expected):
+            assert getattr(obj(), attrname)(*args) == expected
 
         return list(collector._genfunctions('test_' + obj.__module__, test))
 
